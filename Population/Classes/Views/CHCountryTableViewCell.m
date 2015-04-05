757492b31ec9ac14a69bc736fb947b687e579563
Rui Barbosa <rpbarbosa@gmail.com>
2015-04-05 07:33:50 +0100

    Set up country cell wtih autolayout

--- Population/Classes/Views/CHCountryTableViewCell.m
+++ Population/Classes/Views/CHCountryTableViewCell.m
@@ -18,6 +18,9 @@
 @property (nonatomic, strong) UILabel *countryRankLabel;
 @property (nonatomic, strong) UIImageView *countryFlagImageView;
 
+@property (nonatomic, strong) NSArray *views;
+@property (nonatomic, strong) NSDictionary *viewsDictionary;
+
 @end
 
 
@@ -37,18 +40,32 @@
         self.countryRankLabel = [self getCountryRankLabel];
         self.countryFlagImageView = [self getCountryImageView];
         
-        // Do autolayout
+        [self createArrayWithViews];
+        [self addViewsToCell];
         
+        [self removeAutoConstraints];
+
+        [self createViewsDictionary];
+        
+        [self cellAutolayout];
     }
     
     return self;
 }
 
+
+
 #pragma mark - set up cell with country
 
 - (void)setUpWithCountry:(CHCountry *)country
 {
+    NSLog(@"COuntry: %@", country);
     self.countryNameLabel.text = country.name;
+    
+    // Test
+    self.countryPopulationLabel.text = @"123,456,789";
+    self.countryRankLabel.text = @"10";
+    self.countryFlagImageView.image = [UIImage imageNamed:@"Apple-icon"];
 
 }
 
@@ -103,6 +120,140 @@
     imageView.clipsToBounds = YES;
     
     return imageView;
+}
+
+
+
+#pragma mark - Views array
+
+- (void)createArrayWithViews
+{
+    self.views = @[self.countryNameLabel,
+                   self.countryPopulationLabel,
+                   self.countryRankLabel,
+                   self.countryFlagImageView];
+}
+
+
+
+#pragma mark - Add views
+
+- (void)addViewsToCell
+{
+    for (UIView *view in self.views) {
+        [self.contentView addSubview:view];
+    }
+}
+
+
+
+#pragma mark - Remove constraints
+
+- (void)removeAutoConstraints
+{
+    for (UIView *view in self.views) {
+        view.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+}
+
+
+
+#pragma mark - Views Dictionary
+
+- (void)createViewsDictionary
+{
+    self.viewsDictionary = @{@"nameLabel" : self.countryNameLabel,
+                             @"populationLabel" : self.countryPopulationLabel,
+                             @"rankLabel" : self.countryRankLabel,
+                             @"flagView" : self.countryFlagImageView};
+}
+
+
+
+#pragma mark - Autolayout
+
+- (void)cellAutolayout
+{
+    NSArray *constraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[flagView(90)]-[nameLabel]"
+                                            options:0
+                                            metrics:nil
+                                              views:self.viewsDictionary];
+    
+    [self.contentView addConstraints:constraints];
+    
+    constraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[flagView(60)]-|"
+                                            options:0
+                                            metrics:nil
+                                              views:self.viewsDictionary];
+    
+    [self.contentView addConstraints:constraints];
+    
+    
+    constraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:[rankLabel]-|"
+                                            options:0
+                                            metrics:nil
+                                              views:self.viewsDictionary];
+    
+    [self.contentView addConstraints:constraints];
+    
+    
+    constraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:[populationLabel(nameLabel)]"
+                                            options:0
+                                            metrics:nil
+                                              views:self.viewsDictionary];
+    
+    [self.contentView addConstraints:constraints];
+    
+
+    constraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"V:[nameLabel]-10-[populationLabel]"
+                                            options:0
+                                            metrics:nil
+                                              views:self.viewsDictionary];
+    
+    [self.contentView addConstraints:constraints];
+    
+    
+    // Align name and population label
+    NSLayoutConstraint *constraint =
+    [NSLayoutConstraint constraintWithItem:self.countryPopulationLabel
+                                 attribute:NSLayoutAttributeLeft
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.countryNameLabel
+                                 attribute:NSLayoutAttributeLeft
+                                multiplier:1.0
+                                  constant:0.0];
+    
+    [self.contentView addConstraint:constraint];
+    
+    // Center rank with flag
+    constraint =
+    [NSLayoutConstraint constraintWithItem:self.countryRankLabel
+                                 attribute:NSLayoutAttributeCenterY
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.countryFlagImageView
+                                 attribute:NSLayoutAttributeCenterY
+                                multiplier:1.0
+                                  constant:0.0];
+    
+    [self.contentView addConstraint:constraint];
+    
+    // Align vertical title with flag
+    constraint =
+    [NSLayoutConstraint constraintWithItem:self.countryNameLabel
+                                 attribute:NSLayoutAttributeTop
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.countryFlagImageView
+                                 attribute:NSLayoutAttributeTop
+                                multiplier:1.0
+                                  constant:0.0];
+    
+    [self.contentView addConstraint:constraint];
+    
 }
 
 @end
